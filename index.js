d6fd4de7785884af0279c65d9e3ccf95d93186b9
root <root@Ty-Laptop.localdomain>
2022-11-12 12:56:37 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce(function(total, batteryBatches) {
+  return total + batteryBatches;
+}, 0) 
+
+
+console.log(totalBatteries);
